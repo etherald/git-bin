23fad9b8b7ffcd1d553e346d2e6011f4847f6c72
etherald <etherald@dvnty.net>
2015-01-28 20:29:05 -0500

    functionat

---
+++ randomwords.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+# being a perl port of
+#!/bin/sh
+# for i in $@
+#     do
+#     shuf /usr/share/dict/words | head -n $i
+#     done
+
+# january 28 2015 20:19:55 est
+
+use strict;
+use warnings;
+
+sub random_line {
+    my $line;
+    my $path = shift
+    open(my $fh, '<:encoding(UTF-8)', $path) or die " could not open $path";
+    srand;
+    rand($.) < 1 && ($line = $_) while <$fh>;
+    # $line is the random line
+    $line;
+}
+
+print random_line '/usr/share/dict/words';
