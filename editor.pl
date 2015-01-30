1f63d595b4a092d49f3fc86708978ff1dd165e04
etherald <etherald@dvnty.net>
2015-01-29 20:24:26 -0500

    no border

--- editor.pl
+++ editor.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
 # ----------------------------------------------------------------------
 # Script: editor
@@ -12,10 +12,13 @@
 # ----------------------------------------------------------------------
 
 use strict;
+use warnings;
 use Curses;
+use Curses::UI;
+use Curses::UI::Searchable;
 use Cwd;
 
-# Use the libraries from the distribution, instead of 
+# Use the libraries from the distribution, instead of
 # system wide libraries.
 use FindBin;
 use lib "$FindBin::RealBin/../lib";
@@ -24,17 +27,17 @@ use lib "$FindBin::RealBin/../lib";
 # If the file can't be found, assume that this is a new file.
 my $text = "";
 my $currentfile = shift;
-if (defined $currentfile and -f $currentfile) 
+if (defined $currentfile and -f $currentfile)
 {
-	open F, "<$currentfile" or die "Can't read $currentfile: $!\n";
-	while (<F>) { $text .= $_ }
-	$currentfile = $currentfile;
-	close F;
+    open F, "<$currentfile" or die "Can't read $currentfile: $!\n";
+    while (<F>) { $text .= $_ }
+    $currentfile = $currentfile;
+    close F;
 }
 
 # We don't want STDERR output to clutter the screen.
 #
-# Hint: If you need STDERR, write it out to a file and put 
+# Hint: If you need STDERR, write it out to a file and put
 # a tail on that file to see the STDERR output. Example:
 #open STDERR, ">>/tmp/editor_errors.$$";
 open STDERR, ">/dev/null";
@@ -44,148 +47,163 @@ open STDERR, ">/dev/null";
 # ----------------------------------------------------------------------
 
 my @menu = (
-  { -label => 'File', 
-    -submenu => [
-      { -label => 'Open file ^O', -value => \&open_dialog  },
-      { -label => 'Save file ^S', -value => \&save_dialog  },
-      { -label => 'Exit      ^Q', -value => \&exit_dialog  }
-    ]
-  },
-  { -label => 'Help', 
-    -submenu => [
-      { -label => 'About editor', -value => \&about_dialog }
-    ]
-  } 
-);
+    { -label => 'File',
+      -submenu => [
+           { -label => 'Open file ^O', -value => \&open_dialog  },
+           { -label => 'Save file ^S', -value => \&save_dialog  },
+           { -label => 'Exit      ^Q', -value => \&exit_dialog  }
+          ]
+    },
+    { -label => 'Search',
+      -submenu => [
+           { -label => 'Find ^F', -value => \&search }
+          ]
+    },
+    { -label => 'Help',
+      -submenu => [
+           { -label => 'About editor', -value => \&about_dialog }
+          ]
+    }
+    );
 
 # ----------------------------------------------------------------------
 # Create widgets
 # ----------------------------------------------------------------------
 
 # Create the root. Everything else will be built up from here.
-use Curses::UI;
-my $cui = new Curses::UI ( 
-	-clear_on_exit => 1 
-);
+
+my $cui = new Curses::UI (
+    -clear_on_exit => 1
+    );
 
 # Add the menu to the root.
 my $menu = $cui->add(
-	'menu','Menubar', 
-	-menu => \@menu,
-);
+    'menu','Menubar',
+    -menu => \@menu,
+    );
 
 # Create the screen for the editor.
 my $screen = $cui->add(
-	'screen', 'Window',
-	-padtop          => 1, # leave space for the menu
-	-border		 => 0,
-	-ipad		 => 0,
-);
+    'screen', 'Window',
+    -padtop          => 1, # leave space for the menu
+    -border      => 0,
+    -ipad        => 0,
+    );
 
 # We add the editor widget to this screen.
 my $editor = $screen->add(
-	'editor', 'TextEditor',
-	-border 	 => 1,
-	-padtop		 => 0,	
-	-padbottom 	 => 3,
-	-showlines	 => 0,
-	-sbborder	 => 0,
-	-vscrollbar	 => 1,
-	-hscrollbar	 => 1,
-	-showhardreturns => 0,
-	-wrapping        => 0, # wrapping slows down the editor :-(
-	-text		 => $text,
-);
+    'editor', 'TextEditor',
+    -border      => 0,
+    -padtop      => 0,
+    -padbottom   => 3,
+    -showlines   => 0,
+    -sbborder    => 0,
+    -vscrollbar  => 1,
+    -hscrollbar  => 1,
+    -showhardreturns => 0,
+    -wrapping        => 0, # wrapping slows down the editor :-(
+    -text        => $text,
+    #-readonly    => 1
+    );
 
 # There is no need for the editor widget to loose focus, so
 # the "loose-focus" binding is disabled here. This also enables the
 # use of the "TAB" key in the editor, which is nice to have.
 $editor->clear_binding('loose-focus');
 
-# Help information for the user. 
+# Help information for the user.
 $screen->add(
-	'help', 'Label',
-	-y 	 	 => -2,
-	-width		 => -1,
-	-reverse 	 => 1,
-	-paddingspaces   => 1,
-	-text 	 	 => 
-	      " ^Q Quit from the program ^S save file"
-	    . " ^W toggle wrapping\n"
-	    . " ^X Open the menu         ^O open file"
-	    . " ^R toggle hard returns viewing",
-);
+    'help', 'Label',
+    -y       => -2,
+    -width       => -1,
+    -reverse     => 1,
+    -paddingspaces   => 1,
+    -text        =>
+    " ^Q Quit from the program ^S save file"
+    . " ^W toggle wrapping\n"
+    . " ^X Open the menu         ^O open file"
+    . " ^R toggle hard returns viewing",
+    );
 
 # ----------------------------------------------------------------------
 # Callback routines
 # ----------------------------------------------------------------------
-	
+
 sub open_dialog()
 {
-	my $file = $cui->loadfilebrowser( 
-		-file         => $currentfile,
-	);
+    my $file = $cui->loadfilebrowser(
+        -file         => $currentfile,
+        );
 
-	if (defined $file) 
-	{
-		if (open F, "<$file") {
-		    my $text = "";
-		    while (<F>) { $text .= $_ }
-		    close F;
-		    $editor->text($text);
-		    $editor->cursor_to_home;
-	 	    $currentfile = $file;
-		} else { 
-		    $cui->error(-message => "Can't read file \"$file\":\n$!");
-		}
-	}
+    if (defined $file)
+    {
+        if (open F, "<$file") {
+            my $text = "";
+            while (<F>) { $text .= $_ }
+            close F;
+            $editor->text($text);
+            $editor->cursor_to_home;
+            $currentfile = $file;
+        } else {
+            $cui->error(-message => "Can't read file \"$file\":\n$!");
+        }
+    }
 }
 
 sub save_dialog()
 {
 
-	my $file = $cui->savefilebrowser(
-		-file         => $currentfile,
-	);
-	return unless defined $file;
+    my $file = $cui->savefilebrowser(
+        -file         => $currentfile,
+        );
+    return unless defined $file;
 
-	if (open F, ">$file") {
-	    print F $editor->text;
-	    if (close F) {
-		$cui->dialog(-message => "File \"$file\"\nsuccessfully saved");
-		$currentfile = $file;
-	    } else {
-		$cui->error(-message => "Error on closing file \"$file\":\n$!");
-	    }
-	} else {
-	    $cui->error(-message => "Can't write to $file:\n$!");
-	}
+    if (open F, ">$file") {
+        print F $editor->text;
+        if (close F) {
+            $cui->dialog(-message => "File \"$file\"\nsuccessfully saved");
+            $currentfile = $file;
+        } else {
+            $cui->error(-message => "Error on closing file \"$file\":\n$!");
+        }
+    } else {
+        $cui->error(-message => "Can't write to $file:\n$!");
+    }
 }
 
 sub about_dialog()
 {
-	$cui->dialog(
-		-title => 'About editor',
-		-message => "Program : Curses::UI Editor\n"
-	 		  . "Author  : Maurice Makaay\n"
-			  . "\n"
-			  . "The sole purpose of this editor\n"
-			  . "is the demonstration of my perl\n"
-		 	  . "Curses::UI widget set."
-	);
+    $cui->dialog(
+        -title => 'About editor',
+        -message => "Program : Curses::UI Editor\n"
+        . "Author  : Maurice Makaay\n"
+        . "\n"
+        . "The sole purpose of this editor\n"
+        . "is the demonstration of my perl\n"
+        . "Curses::UI widget set."
+        );
 }
-		
+
 sub exit_dialog()
 {
-	my $return = $cui->dialog(
-			-title     => "Are you sure???", 
-			-buttons   => ['yes', 'no'],
-			-message => "Do you really want to quit?"
-	);
+    my $return = $cui->dialog(
+        -title     => "Are you sure???",
+        -buttons   => ['yes', 'no'],
+        -message => "Do you really want to quit?"
+        );
 
-	exit(0) if $return;
+    exit(0) if $return;
 }
 
+# sub search() {
+#     $cui->search;
+# }
+
+sub toggle_readonly {
+    #$cui->readonly(!$cui->{-readonly});
+    $cui->readonly(1);
+    $cui->layout_content;
+}
 
 # ----------------------------------------------------------------------
 # The main loop of the program
@@ -195,16 +213,19 @@ sub exit_dialog()
 $cui->set_binding(\&exit_dialog, "\cQ", "\cC");
 $cui->set_binding(\&save_dialog, "\cS");
 $cui->set_binding(\&open_dialog, "\cO");
+#$cui->set_binding($cui->toggle_readonly, "\cT");
+#$cui->set_binding($cui->search, "\cF");
+#$cui->set_routine('search'  , $cui->search);
+#$cui->set_binding('search-forward'  , '\cF');
 $cui->set_binding(sub {shift()->getobj('menu')->focus}, "\cX", KEY_F(10));
 $cui->set_binding(sub {
-	my $cui = shift;
-	$cui->layout;
-	$cui->draw;
-}, "\cL");
+    my $cui = shift;
+    $cui->layout;
+    $cui->draw;
+                  }, "\cL");
 
 
 # Bring the focus to the editor widget.
 $editor->focus;
 
 $cui->mainloop;
-
