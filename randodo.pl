cb081018fba396111057bc1ffa6e98932f40f9e7
Etherald <web@etherald.com>
2016-02-23 16:31:40 +0000

    file map

---
+++ randodo.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Cwd;
+use File::Random qw/random_file/;
+use File::Type;
+
+
+sub randomfile {
+    my $pat = '^.+\.txt$'; #TODO: use file magic somehow to detect text files without extension
+
+    #my @dirs = ((shift @ARGV) or cwd());
+    #my @dirs = qw($ENV{'HOME'} . '/txt');
+    my $dir = $ENV{'HOME'} . '/txt/';
+    print $dir;
+    my $random_file = random_file(-dir       => $dir,
+                                  -check => sub { my $ft = File::Type->new()->checktype_filename() =~ /^text\//; },
+                                  -recursive => 1);
+    print "$random_file\n";
+
+}
+
+
+randomfile;
