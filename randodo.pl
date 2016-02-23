7684629da486e9c2d9fe0b00a94d51e098c3f03b
Etherald <web@etherald.com>
2016-02-23 16:56:07 +0000

    cu

--- randodo.pl
+++ randodo.pl
@@ -11,16 +11,10 @@ use constant DIR => $ENV{'HOME'} . '/txt/';
 my $dir = $ENV{'HOME'} . '/txt/';
 
 sub randomfile {
-    my $pat = '^.+\.txt$'; #TODO: use file magic somehow to detect text files without extension
-
-    #my @dirs = ((shift @ARGV) or cwd());
-    #my @dirs = qw($ENV{'HOME'} . '/txt');
-    #my $dir = $ENV{'HOME'} . '/txt/';
     my $random_file = random_file(-dir       => $dir,
                                   -check => sub { File::Type->new()->checktype_filename($dir.$_) =~ /^text\//; },
                                   -recursive => 1);
     #print "$random_file\n";
-
 }
 
 sub dododo {
